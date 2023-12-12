3a9b71e165f5772e5ebd3fde28731524dd3f55f1
pvnick <pvnick@yandex.ru>
2023-12-13 00:16:02 +0700

    через include добавил url приложения macro_gallery и настройки media

--- macro_site/macro_site/urls.py
+++ macro_site/macro_site/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+# настройки для отображения в админке вместо слов "Администрирование" и др.
+admin.site.site_header = 'МакроЭтоЗбс!'
+admin.site.index_title = 'Не устану повторять! Вы - прекрасный администратор'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('macro_gallery.urls', namespace='macro_gallery')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+        )
