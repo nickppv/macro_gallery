751ed934796b6b17036ab764de741f926a7dab98
pvnick <pvnick@yandex.ru>
2023-12-13 00:06:03 +0700

    добавлена модель для фотографий

--- macro_site/macro_gallery/models.py
+++ macro_site/macro_gallery/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Фоточки'
