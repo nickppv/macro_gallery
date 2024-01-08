90be90ed1a6d8a36fd7c4811a795dd7ca4a9a9e4
pvnick <pvnick@yandex.ru>
2024-01-09 03:48:30 +0700

    добавлена система тегирования

--- macro_site/macro_gallery/models.py
+++ macro_site/macro_gallery/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from taggit.managers import TaggableManager
+
 
 class Photo(models.Model):
     title = models.CharField(max_length=100)
@@ -7,6 +9,7 @@ class Photo(models.Model):
     created_at = models.DateTimeField(blank=True, null=True)
     published_date = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
