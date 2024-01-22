da4358cdfba95199d85b60dd96de0f5b4a7c199d
pvnick <pvnick@yandex.ru>
2024-01-22 17:40:40 +0700

    вместо пути в админке теперь отображаются миниатюры изображений, переведено название приложения 'macro_gallery' на русский

--- macro_site/macro_gallery/apps.py
+++ macro_site/macro_gallery/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MacroGalleryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'macro_gallery'
+    verbose_name = "Галерея макрофотографий"  # изменили имя приложения в админке на русское
