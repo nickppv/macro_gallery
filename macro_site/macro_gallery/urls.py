8e3996e47d6a8b7f6070559fc1d00602e3383342
pvnick <pvnick@yandex.ru>
2024-02-08 07:43:42 +0700

    добавлен путь для страницы поиска из формы (по названию, описанию и тегам)

--- macro_site/macro_gallery/urls.py
+++ macro_site/macro_gallery/urls.py
@@ -6,4 +6,5 @@ app_name = 'macro_gallery'
 urlpatterns = [
     path('', views.index, name='index'),
     path('tag/<slug:tag_slug>/', views.index, name='result_by_tag'),
+    path('search/', views.search, name='search'),
 ]
