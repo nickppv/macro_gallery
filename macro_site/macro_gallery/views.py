8c1d6aae28e118f313ec72d5cb301682163476aa
pvnick <pvnick@yandex.ru>
2024-02-08 07:42:12 +0700

    рандомная сортировка и изменил название переменной для вывода всех функций на один шаблон - теперь он один

--- macro_site/macro_gallery/views.py
+++ macro_site/macro_gallery/views.py
@@ -1,8 +1,9 @@
-import json
 from random import sample
 from PIL import Image
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.contrib.postgres.search import SearchVector
+from .forms import SearchForm
 from .models import Photo
 from taggit.models import Tag
 
@@ -35,49 +36,40 @@ def index(request, tag_slug=None):
         tag = get_object_or_404(Tag, slug=tag_slug)
         all_photo = all_photo.filter(tags__in=[tag])
     # случайная выборка изображений для слайдера
+    results = all_photo.order_by('?')
 
     return render(request,
                   'macro_gallery/index.html',
-                  {
-                   'all_photo': all_photo,
-                   'slider_photo_list': slider_photo_list,
-                  })
+                  {'title': 'Главная страница',
+                   'results': results,
+                   'slider_photo_list': slider_photo_list, })
 
 
+def search(request):
+    all_photo = Photo.objects.all()
+    slider_photo_list = []
+    for i in all_photo:
+        if choose_picture_for_slider(i):
+            slider_photo_list.append(i)
+    slider_photo_list = sample(slider_photo_list, 5)
 
+    form = SearchForm()
+    query = None
+    results = []
 
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Photo.objects.annotate(
+                search=SearchVector('title', 'description', 'tags__name'),
+                ).filter(search=query).values('id').distinct()
+            results = Photo.objects.filter(id__in=results).order_by('?')
 
-# def choose_picture_for_slider(lst: list) -> list:
-#     '''
-#     выбираем изображения с пейзажной ориентацией
-#     '''
-#     result_list: list = []
-#     for i in lst:
-#         image = Image.open('./' + i)
-#         w, h = image.size
-#         if w > h:
-#             result_list.append(i)
-#         image.close()
-#     return result_list
-
-# def index(request, tag_slug=None):
-#     all_photo = Photo.objects.all()
-
-#     # создаем поиск по тегу
-#     tag = None
-#     if tag_slug:
-#         tag = get_object_or_404(Tag, slug=tag_slug)
-#         all_photo = all_photo.filter(tags__in=[tag])
-#     # создаем список путей для картинок верхней галереи
-#     slider_image = Photo.objects.all().values_list('photo', flat=True)
-#     path_to_picture = ['media/' + i for i in slider_image]
-#     # случайная выборка изображений для слайдера
-#     images_for_slider = sample(choose_picture_for_slider(path_to_picture), 5)
-
-#     return render(request,
-#                   'macro_gallery/index.html',
-#                   {
-#                    'all_photo': all_photo,
-#                 #    'images_top_gallery': json.dumps(path_to_picture),
-#                    'images_for_slider': images_for_slider,
-#                   })
+    return render(request,
+                  'macro_gallery/index.html',
+                  {'title': f'Поиск по: {query}',
+                   'form': form,
+                   'query': query,
+                   'results': results,
+                   'slider_photo_list': slider_photo_list, })
